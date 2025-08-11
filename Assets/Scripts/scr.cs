1944167c69de82a54c18630985474c0bceaf215d
drxravehub <r.orazymbetov@tttu.edu.kz>
2025-08-12 01:50:07 +0500

    added new prefabs and some input

--- Assets/Scripts/scr.cs
+++ Assets/Scripts/scr.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 public class scr : MonoBehaviour
 {
+    public float speed = 5.0f;
+    private Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        
+        float g = Input.GetAxis("Horizontal");
+        float e = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(g, 0f, e);
+        rb.linearVelocity = movement * speed;
     }
 }
